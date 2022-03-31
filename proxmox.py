24d1316590047b62fd391a936543e9163631a515
erraata <43318289+erraata@users.noreply.github.com>
2022-03-31 18:57:58 -0500

    Fix template error

--- proxmox.py
+++ proxmox.py
@@ -39,7 +39,7 @@ class ProxmoxVMList(list):
 
     def get_names(self):
         if self.ver >= 4.0:
-            return [vm['name'] for vm in self if vm['template'] != 1]
+            return [vm['name'] for vm in self if vm.get('template') != 1]
         else:
             return [vm['name'] for vm in self]
 
